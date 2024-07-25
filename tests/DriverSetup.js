36dba631f6c99c8e0cb3d248410dd08df2bce2da
Gabriel Castilho <gabriel.castilho@outlook.com.br>
2024-07-25 18:30:15 -0300

    adicionado opção para rodar o chrome visualmente na maquina

--- tests/DriverSetup.js
+++ tests/DriverSetup.js
@@ -5,14 +5,16 @@ let driverInstance;
 
 export async function getDriver() {
   if (!driverInstance) {
+    
     let options = new chrome.Options();
     options.addArguments('--headless');
     options.addArguments('--no-sandbox');
-    options.addArguments('--disable-dev-shm-usage');
+   options.addArguments('--disable-dev-shm-usage');
     options.addArguments('--disable-gpu');
     options.addArguments('--window-size=1920,1080');
-    
     driverInstance = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+    
+    //driverInstance = await new Builder().forBrowser('chrome').build()
   }
   return driverInstance;
 }
