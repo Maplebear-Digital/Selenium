a8068620948c0bfb0f6c08fad9a5a95666b0e114
Gabriel Castilho <gabriel.castilho@outlook.com.br>
2024-07-25 18:09:05 -0300

    corrigindo driver

--- tests/DriverSetup.js
+++ tests/DriverSetup.js
@@ -1,10 +1,18 @@
-import { Builder } from 'selenium-webdriver';
+import { Builder, Capabilities } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
 
 let driverInstance;
 
 export async function getDriver() {
   if (!driverInstance) {
-    driverInstance = await new Builder().forBrowser('chrome').build();
+    let options = new chrome.Options();
+    options.addArguments('--headless');
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-dev-shm-usage');
+    options.addArguments('--disable-gpu');
+    options.addArguments('--window-size=1920,1080');
+    
+    driverInstance = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
   }
   return driverInstance;
 }
