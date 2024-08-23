184a2cfff4fbf08c36100f26244b01146add5534
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-08-23 16:08:56 -0300

    test

--- tests/Users/RegisterStudentTest.js
+++ tests/Users/RegisterStudentTest.js
@@ -7,7 +7,7 @@ let driver;
 let registerStudent;
 
 describe('Register Student Test', function() {
-  this.timeout(30000);
+  this.timeout(60000);
 
   before(async function() {
     driver = await getDriver();
