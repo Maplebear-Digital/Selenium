1b75303a73be62a6029847ae23afa5c9e87fcf1b
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-08-23 17:48:05 -0300

    test

--- tests/selenium-test.js
+++ tests/selenium-test.js
@@ -1,6 +1,6 @@
 
 import './Login/LoginTest.js'
-import './Users/RegisterStudentTest.js';
-import './Users/RegisterResponsibleTest.js';
+// import './Users/RegisterStudentTest.js';
+// import './Users/RegisterResponsibleTest.js';
 // import './Cart/CartTest.js';
 // import './Checkout/PaymentTest.js';
