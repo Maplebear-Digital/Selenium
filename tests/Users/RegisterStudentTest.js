9576f42a7a68e254d06dfcbfe66d229657ad6bd2
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-08-23 17:33:40 -0300

    test

--- tests/Users/RegisterStudentTest.js
+++ tests/Users/RegisterStudentTest.js
@@ -7,7 +7,7 @@ let driver;
 let registerStudent;
 
 describe('Register Student Test', function() {
-  this.timeout(100000);
+  this.timeout(30000);
 
   before(async function() {
     driver = await getDriver();
@@ -23,14 +23,23 @@ describe('Register Student Test', function() {
   it('should register student', async function() {
     try {
       await registerStudent.createStudent();
+      console.log('createStudent')
       await registerStudent.selectSchool();
+      console.log('selectSchool')
       await registerStudent.insertNameStudent('Gabriel Oliveira');
+      console.log('insertNameStudent')
       await registerStudent.birthdayDate('17102017');
+      console.log('birthdayDate')
       await registerStudent.selectGender();
+      console.log('selectGender')
       await registerStudent.clickButtonToContinueToCourses();
+      console.log('clickButtonToContinueToCourses')
       await registerStudent.selectYear();
+      console.log('selectYear')
       await registerStudent.selectGrade();
+      console.log('selectGrade')
       await registerStudent.clickButtonRegisterStudent();
+      console.log('clickButtonRegisterStudent')
 
       let isStudentNameCorrect = await registerStudent.verifyStudent('Gabriel Oliveira');
       assert.strictEqual(isStudentNameCorrect, true, 'Student name is not displayed correctly');
