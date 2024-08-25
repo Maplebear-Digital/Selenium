598248e0d78156e5cacd5195b3d48164669b2c06
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-08-25 18:15:15 -0300

    fix

--- tests/Admin/AdminTest.js
+++ tests/Admin/AdminTest.js
@@ -1,4 +1,4 @@
-const { Builder, By, until } = require('selenium-webdriver');
+import { Builder, By, until } from 'selenium-webdriver';
 import saveScreenshot from '../../functions/screenshot.js'
 
 (async function loginMagentoAdmin() {
@@ -13,12 +13,12 @@ import saveScreenshot from '../../functions/screenshot.js'
     await driver.wait(until.elementLocated(By.id('username')), 10000);
 
     // Preencha o campo de nome de usuário
-    await driver.findElement(By.id('username')).sendKeys('viniciush.oliveira ');
+    await driver.findElement(By.id('username')).sendKeys('your-admin-username');
 
     // Preencha o campo de senha
-    await driver.findElement(By.id('login')).sendKeys('Senha123@@@@');
+    await driver.findElement(By.id('login')).sendKeys('your-admin-password');
 
-    await saveScreenshot(driver, 'screenshots/1_insert_user_login_admin_page.png');
+    await saveScreenshot(driver, 'screenshots/test1.png');
 
     // Clique no botão de login
     await driver.findElement(By.className('action-login')).click();
@@ -26,7 +26,7 @@ import saveScreenshot from '../../functions/screenshot.js'
     // Aguarde até que a página do painel seja carregada
     await driver.wait(until.titleIs('Dashboard / Magento Admin'), 10000);
 
-    await saveScreenshot(driver, 'screenshots/1_admin_should_open.png');
+    await saveScreenshot(driver, 'screenshots/test2.png');
 
     console.log('Login realizado com sucesso!');
   } catch (error) {
