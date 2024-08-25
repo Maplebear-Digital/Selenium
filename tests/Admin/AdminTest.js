14a8ca3dd93a67416855b33cb6bb2be0ff93e272
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-08-25 18:22:02 -0300

    fix

--- tests/Admin/AdminTest.js
+++ tests/Admin/AdminTest.js
@@ -1,38 +1,22 @@
 import { Builder, By, until } from 'selenium-webdriver';
 import saveScreenshot from '../../functions/screenshot.js'
 
-(async function loginMagentoAdmin() {
-  // Crie uma nova instância do navegador
+export async function loginMagentoAdmin() {
   let driver = await new Builder().forBrowser('chrome').build();
 
   try {
-    // Acesse a página de login do Magento 2
-    await driver.get('https://b2c-staging.maplebearstore.com.br/painel'); // Substitua pelo URL do seu site Magento
-
-    // Aguarde até que o campo de login seja carregado
+    await driver.get('https://b2c-staging.maplebearstore.com.br/painel');
     await driver.wait(until.elementLocated(By.id('username')), 10000);
-
-    // Preencha o campo de nome de usuário
     await driver.findElement(By.id('username')).sendKeys('your-admin-username');
-
-    // Preencha o campo de senha
     await driver.findElement(By.id('login')).sendKeys('your-admin-password');
-
-    await saveScreenshot(driver, 'screenshots/test1.png');
-
-    // Clique no botão de login
+    await saveScreenshot(driver, 'screenshots/teste.png');
     await driver.findElement(By.className('action-login')).click();
-
-    // Aguarde até que a página do painel seja carregada
     await driver.wait(until.titleIs('Dashboard / Magento Admin'), 10000);
-
-    await saveScreenshot(driver, 'screenshots/test2.png');
 
     console.log('Login realizado com sucesso!');
   } catch (error) {
     console.error('Erro ao tentar realizar login:', error);
   } finally {
-    // Feche o navegador
     await driver.quit();
   }
-})();
+}
