a2a437c850de4af0a52a849f0c25103b4d80aca4
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-08-26 14:35:48 -0300

    fix

--- tests/generate-pdf.js
+++ tests/generate-pdf.js
@@ -8,22 +8,36 @@ const outputPDF = path.join(screenshotsDir, 'test-results.pdf');
 const doc = new PDFDocument();
 doc.pipe(fs.createWriteStream(outputPDF));
 
+// Página inicial
+doc.fontSize(26)
+    .text('Report de teste', { align: 'center' })
+    .moveDown(0.5)
+    .fontSize(20)
+    .text('Testes no Magento', { align: 'center' });
+
+doc.addPage(); // Adiciona uma nova página para as imagens
+
+// Filtra os arquivos de imagem
 const imageFiles = fs.readdirSync(screenshotsDir).filter(file => path.extname(file) === '.png');
 
 imageFiles.forEach((file, index) => {
     const filePath = path.join(screenshotsDir, file);
 
     if (index > 0) {
-        doc.addPage(); // Adiciona uma nova página após a primeira
+        doc.addPage(); // Adiciona uma nova página para cada imagem, exceto a primeira
     }
 
     const title = path.basename(file, path.extname(file));
-    doc.fontSize(18).text(title, { align: 'center', underline: true }); // Título centralizado e sublinhado
+
+    // Define a cor do título como verde
+    doc.fillColor('green')
+        .fontSize(18)
+        .text(title, { align: 'center', underline: true }); // Título centralizado e sublinhado
 
     doc.moveDown(); // Adiciona um espaço entre o título e a imagem
 
     doc.image(filePath, {
-        fit: [500, 400], // Ajuste o tamanho da imagem conforme necessário
+        fit: [500, 400], // Ajusta o tamanho da imagem conforme necessário
         align: 'center',
         valign: 'center'
     });
