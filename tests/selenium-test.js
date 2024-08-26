af0d536a1ce5695a75135caa023da24c5b5a30c2
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-08-26 15:54:43 -0300

    fix

--- tests/selenium-test.js
+++ tests/selenium-test.js
@@ -1,7 +1,7 @@
 
 import './Login/LoginTest.js'
 import './Admin/Sales/SalesTest.js'
-import './Users/RegisterStudentTest.js';
+// import './Users/RegisterStudentTest.js';
 // import './Users/RegisterResponsibleTest.js';
 // import './Cart/CartTest.js';
 // import './Checkout/PaymentTest.js';
