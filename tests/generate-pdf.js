02b7262ba03a0176ced06e3c2f833f96b4136db8
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-09-01 15:18:44 -0300

    improve

--- tests/generate-pdf.js
+++ tests/generate-pdf.js
@@ -13,7 +13,10 @@ doc.fontSize(26)
     .text('Report de Teste', { align: 'center' })
     .moveDown(0.5)
     .fontSize(20)
-    .text('Testes na Maplebear Store by Magento 2.4.6', { align: 'center' });
+    .text('Testes na Maplebear Store by Magento 2.4.6', { align: 'center' })
+    .moveDown(0.5)
+    .fontSize(16)
+    .text(formatDateAndTime(), { align: 'center' });
 
 doc.addPage(); // Adiciona uma nova página para as imagens
 
@@ -45,3 +48,26 @@ imageFiles.forEach((file, index) => {
 
 doc.end();
 console.log(`PDF gerado em ${outputPDF}`);
+
+function formatDateAndTime() {
+    const now = new Date();
+
+    // Formatar a data no formato d/M/Y
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Meses são de 0-11
+    const year = now.getFullYear();
+
+    const formattedDate = `${day}/${month}/${year}`;
+
+    // Formatar a hora no formato hh:mm:ss
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    // Gerar a mensagem final
+    const message = `Testes realizados em ${formattedDate} às ${formattedTime}`;
+
+    return message;
+}
